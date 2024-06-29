00cb5f28b02c7597f05a889145416765059a987a
Gavin Guinn <gavinguinn1@gmail.com>
2024-06-28 18:33:50 -0600

    working at commands

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -65,6 +65,10 @@ void Error_Handler(void);
 #define USART_RX_GPIO_Port GPIOA
 #define LD2_Pin GPIO_PIN_5
 #define LD2_GPIO_Port GPIOA
+#define WIFI_TX_Pin GPIO_PIN_9
+#define WIFI_TX_GPIO_Port GPIOA
+#define WIFI_RX_Pin GPIO_PIN_10
+#define WIFI_RX_GPIO_Port GPIOA
 #define TMS_Pin GPIO_PIN_13
 #define TMS_GPIO_Port GPIOA
 #define TCK_Pin GPIO_PIN_14
