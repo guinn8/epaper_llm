d4392c04c1eab99f2b3ef2a288f7c453b1890740
Gavin Guinn <gavinguinn1@gmail.com>
2024-07-05 14:15:26 -0600

    async server

--- scripts/server.py
+++ scripts/server.py
@@ -1,4 +1,18 @@
 import socket
+import threading
+
+def handle_client(client_socket, client_address):
+    with client_socket:
+        print(f"Connection from {client_address}")
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            message = data.decode('utf-8')
+            print(f"Received data: {message}")
+            response = "pong"
+            print(f"Sending: {response.encode('utf-8')}")
+            client_socket.sendall(response.encode('utf-8'))
 
 def start_server(host='0.0.0.0', port=8080):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
@@ -8,17 +22,8 @@ def start_server(host='0.0.0.0', port=8080):
 
         while True:
             client_socket, client_address = server_socket.accept()
-            with client_socket:
-                print(f"Connection from {client_address}")
-                while True:
-                    data = client_socket.recv(1024)
-                    if not data:
-                        break
-                    message = data.decode('utf-8')
-                    print(f"Received data: {message}")
-                    response = "pong"
-                    print(f"Sending: {response.encode('utf-8')}")
-                    client_socket.sendall(response.encode('utf-8'))
+            client_handler = threading.Thread(target=handle_client, args=(client_socket, client_address))
+            client_handler.start()
 
 if __name__ == "__main__":
     start_server()
